fdbcb5c0f2662192ee6999073e1c44bf021cd92b
haideralyy01 <haideralyy01@gmail.com>
2026-06-26 15:07:20 +0530

    chore(env): add dotenv and fix DATABASE_URL parsing

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import express from 'express';
 import { prismaClient } from "@repo/db"
 
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,10 +1,11 @@
+import "dotenv/config"
 import { WebSocketServer } from 'ws';
 import { prismaClient } from "@repo/db"
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
-    prismaClient.user.create({
+wss.on('connection', async (ws) => {
+    await prismaClient.user.create({
         data: {
             email: Math.random().toString(),
             password: Math.random().toString(),
